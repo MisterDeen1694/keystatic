78d30ee72bec2abfa69beed14da04eacee3c039f
Brandon <jason-deen@outlook.com>
2025-05-26 21:11:27 +0200

    :bug: updated some things

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel";
 import { defineConfig } from "astro/config";
 import keystatic from "@keystatic/astro";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
 	output: "static",
-	adapter: vercel(),
+	adapter: netlify(),
 	integrations: [
 		tailwind({
 			applyBaseStyles: false,
