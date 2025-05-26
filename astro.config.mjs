c64d25ea714ef7a5fdea9dfa25657296ed5da449
Brandon <jason-deen@outlook.com>
2025-05-26 20:54:48 +0200

    Lets pus it to netlifly to test

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel";
+import { defineConfig } from "astro/config";
+import keystatic from "@keystatic/astro";
+
+// https://astro.build/config
+export default defineConfig({
+	output: "static",
+	adapter: vercel(),
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		react(),
+		keystatic(),
+	],
+});
